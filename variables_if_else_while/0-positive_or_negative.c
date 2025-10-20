d1414d079fe39f3cdf50462b48ae1547c6aa0230
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-20 09:53:02 +0000

    FIx : Try new program

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,16 +6,17 @@
 *
 * Return : 0 (success)
 */
+/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positiv \n", n);
+printf("%d is positiv\n", n);
 else if (n == 0)
-printf("%d is zero, \n", n);
+printf("%d is zero,\n", n);
 else
-printf("%d is negativ \n", n);
+printf("%d is negativ\n", n);
 return (0);
 }
