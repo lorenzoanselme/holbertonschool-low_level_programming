0cc9474f10f6a3cad78e126e5491fdf538cd0063
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-20 17:20:30 +0000

    Fix : wrong description

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_alphabet - print alphabet in lowercase
+* print_alphabet_x10 - print alphabet 10 times in lowercase
 * Return : void
 */
 void print_alphabet_x10(void)
