a794183c2d115403145b699d48b0c4a6741baf03
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-20 09:00:43 +0000

    FIx: : write wright script

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,11 @@
-int puts ("\"Programming is like building a multilingual puzzle")
+#include <studio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
