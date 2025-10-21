64007edcbc89ce7a6219dba7dd5d56afcae0ac89
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-21 07:24:03 +0000

    Feat : Add a function that checks for lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return : Always 0
+*
+* c : letter
+*
+*/
+int main(void)
+int c(int)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
