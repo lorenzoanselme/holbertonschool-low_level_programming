14030ec25b155f77f0ef406355a0eca0ae47e532
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-21 07:38:30 +0000

    Feat : Add a function that checks for alphabetic character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabetic charactere
+*
+* @c: The character to check
+*
+* Return: 1 if lowercase, 0 otherwise
+*/
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+return (1);
+}
+else
+{
+return (0);
+}
+}
