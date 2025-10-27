1558991bf86a8882b3efc6401c77a3ad027b0c3d
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-27 09:54:29 +0000

    Fix : put _putchar to the good place

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,18 @@
 void print_line(int n)
 {
 	int i;
+	int n;
 
 	for (i = 0; i <= n; i++)
 	{
 		if (n <= 0)
 		{
-		_putchar('\n');
+			_putchar('\n');
 		}
 		else
 		{
 			_putchar('_');
-			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
