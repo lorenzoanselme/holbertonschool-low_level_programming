9d2d5457415b4ed48f4f8516264e07374d7bd018
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-27 13:12:24 +0000

    Feat : a function that swaps the values of two integers.

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "main.h"
+/**
+ * swap_int - takes a pointer
+ * @a : valeur a
+ * @b : valeur b
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
