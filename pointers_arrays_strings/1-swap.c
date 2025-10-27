c93f2cbef1ab83e06556a71ef50cf66e2be87da7
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-27 13:22:56 +0000

    Feat : Add a function that returns the length of a string.

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "main.h"
 /**
  * swap_int - takes a pointer
  * @a : valeur a
---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - retourne la longueur d'une chaîne de caractères
+ * @s: pointeur vers la chaîne à mesurer
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
