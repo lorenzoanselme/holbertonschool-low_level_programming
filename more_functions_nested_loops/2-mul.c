7614c8a677323c9890ec118a25c37c5fa531ddff
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-27 08:41:54 +0000

    Feat : Add a function that multiplies two integers.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - mul two integers and returns the result
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: the sum of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
