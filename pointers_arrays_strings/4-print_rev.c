f20a491c59238918044c04477b2a31139e2c85c6
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-27 13:51:22 +0000

    Feat : Add a function that prints a string, in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - affiche une chaîne de caractères en ordre inverse
+ * @s: pointeur vers la chaîne à afficher
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c;
+	int i;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+
+	for (i = c; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
