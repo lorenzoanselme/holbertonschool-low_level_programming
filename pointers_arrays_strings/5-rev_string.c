2dbc266807bd5cb3c6f58973789e883559071463
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-27 14:28:04 +0000

    Feat : Add a function that reverses a string.

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - inverse une chaîne de caractères
+ * @s: pointeur vers la chaîne à inverser
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	int length;
+	char temp;
+
+	length = _strlen(s);
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
