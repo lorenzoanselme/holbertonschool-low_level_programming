69b8e94ca8bb59c9ed955581e820b64ab237eb86
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-28 13:35:17 +0000

    Feat : Ajouter la fonction _strncat pour concaténer un nombre spécifié de caractères de deux chaînes

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to concatenate from src
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+	int n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
