396accc8446360013788914eaa8762a41225e92b
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-28 09:05:38 +0000

    Feat : Add a function that prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of a string
+ * @str : pointeur vers la chaîne de caractères
+ * return - void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+	}
+	for (i = c / 2 - 1; i <= c; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
