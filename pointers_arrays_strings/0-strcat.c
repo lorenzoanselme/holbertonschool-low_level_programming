fcefe2bd61598f54023cfb3cd6ba800d625a8618
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-10-28 13:26:00 +0000

    Feat : Ajouter la fonction _strcat pour concaténer deux chaînes de caractères

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
