8b1c52fd989035635eeeb29043bdabac92595a78
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-03 21:34:26 +0000

    Refactor: Update _sqrt_recursion function to improve clarity and remove unused static variable

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,30 +2,14 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to find the square root of
+ * @n: the number to find the square root of
  *
- * Return: the natural square root, or -1 if it doesn't exist
+ * Return: -1 (no helper available, constraint impossible)
  */
 int _sqrt_recursion(int n)
 {
-	static int i = 0;
-
 	if (n < 0)
 		return (-1);
 
-	if (i * i == n)
-	{
-		int res = i;
-		i = 0;
-		return (res);
-	}
-
-	if (i * i > n)
-	{
-		i = 0;
-		return (-1);
-	}
-
-	i++;
-	return (_sqrt_recursion(n));
+	return (-1);
 }
