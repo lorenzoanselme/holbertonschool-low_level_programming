9e6e51d85fe026d689b6cfe29f4e620e37c47783
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-05 10:21:23 +0000

    Feat: Update 0-whatsmyname to use printf and remove unused code; add 1-args to print argument count

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the program
+ * main - prints the number of arguments passed to the program
  * @argc: argument count
  * @argv: argument vector
  *
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
