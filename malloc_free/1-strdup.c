64ff18cd02ce2956b6d2f01b088bc3ae213b6332
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-12 00:45:21 +0000

    Feat: implement _strdup function to duplicate a string using malloc

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - retourne un pointeur vers une nouvelle chaîne dupliquée
+ * @str: chaîne à dupliquer
+ *
+ * Return: pointeur vers la nouvelle chaîne, ou NULL si échec
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+
+	return (dup);
+}
