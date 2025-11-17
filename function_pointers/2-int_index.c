91f5a65194952804bb8ef4a48f2aaf52b6da620d
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-17 13:02:53 +0000

    Feat: add int_index function with comparison callback

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer using a comparison function
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: pointer to the function used to compare values
+ *
+ * Return: index of first matching element, or -1 if none or error
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
