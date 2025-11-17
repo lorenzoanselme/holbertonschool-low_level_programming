ce56d59bb99922d04ac2598448f7f952c8eafeae
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-17 13:05:35 +0000

    Feat: add full calculator program with function pointers

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: int
+ * @b: int
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: int
+ * @b: int
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: int
+ * @b: int
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: int
+ * @b: int
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of two integers
+ * @a: int
+ * @b: int
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
