6a1e3e1c79167e4617038e11e46c17890d5c053d
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-19 22:53:22 +0000

    Feat: implement valid keygen generating ASCII-sum 2772 password for 101-crackme

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints valid password for 101-crackme
+ * main - generates a valid password for 101-crackme
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    printf("RMkrS@7L^PXQi]D");
-    return (0);
+	int sum = 0, i, r;
+	char password[16];
+
+	srand(time(NULL));
+
+	for (i = 0; i < 14; i++)
+	{
+		r = (rand() % 94) + 33;
+		password[i] = r;
+		sum += r;
+	}
+
+	password[14] = 2772 - sum;
+	password[15] = '\0';
+
+	printf("%s", password);
+	return (0);
 }
