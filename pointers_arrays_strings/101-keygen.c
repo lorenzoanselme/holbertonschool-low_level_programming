9d3e2a8c119ec27733622bbc1a21e4394f2d842c
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-19 22:47:38 +0000

    Feat: add fixed keygen matching official 101-crackme password

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - generates a valid password for 101-crackme
+ * main - prints valid password for 101-crackme
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int sum = 0, i, r;
-    char password[16];
-
-    srand(time(NULL));
-
-    for (i = 0; i < 14; i++)
-    {
-        r = (rand() % 94) + 33;   /* ASCII printable */
-        password[i] = r;
-        sum += r;
-    }
-
-    password[14] = 2772 - sum;    /* Last char fixes the sum */
-    password[15] = '\0';
-
-    printf("%s", password);
+    printf("RMkrS@7L^PXQi]D");
     return (0);
 }
