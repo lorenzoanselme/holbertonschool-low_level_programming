3b4fc2c52298f4de2220f9b43bb7feb5dab05ec5
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-11-19 22:31:33 +0000

    Feat: implement variadic print_all function supporting multiple data types

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments
+ *
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, p = 0;
+	char *str, *sep = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+		    format[i] == 'f' || format[i] == 's')
+		{
+			printf("%s", sep);
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%d", va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					printf("(nil)");
+				if (str)
+					printf("%s", str);
+			}
+			sep = ", ";
+		}
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
+}
