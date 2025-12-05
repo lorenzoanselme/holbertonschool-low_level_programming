8f38b6d6182ae15cdcf2e9b6c2e04c4056177a20
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-12-05 22:03:29 +0000

    feat: implement append_text_to_file to append text to an existing file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL-terminated string to add at the end
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, length = 0;
+	ssize_t w;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* Open file in write + append mode, DO NOT create file */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1); /* file doesn't exist OR cannot open */
+
+	/* If text_content is NULL: nothing to write, but success */
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	/* Calculate string length (no strlen allowed) */
+	while (text_content[length])
+		length++;
+
+	w = write(fd, text_content, length);
+	if (w == -1 || w != length)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
